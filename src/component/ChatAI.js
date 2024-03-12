118e3ba491dfd8e75a64b2163f8ba900794686ae
MaiThanhNam141 <maithanhnam141@gmail.com>
2024-03-12 14:05:05 +0700

    Update: Project name, ChatAI, HomeScreen

--- src/component/ChatAI.js
+++ src/component/ChatAI.js
@@ -1,44 +1,56 @@
-import { Text, View, TextInput, Button, ToastAndroid, ScrollView, ActivityIndicator, StyleSheet, TouchableOpacity} from "react-native"
-import React, { useState } from "react"
+import { Text, View, TextInput, Button, ToastAndroid, ActivityIndicator, StyleSheet, FlatList, Image} from "react-native"
+import React, { useState, useEffect } from "react"
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { GoogleGenerativeAI  } from "@google/generative-ai"
 import auth from '@react-native-firebase/auth';
 import {API_KEY} from '@env'
 
-const ChatAI = () => {
-    const [prompt,setPrompt] = useState('')
-    const [text, setText] = useState('')
-    const [email, setEmail] = useState('');
-    const [loading, setLoading] = useState(false)
-    const genAI = new GoogleGenerativeAI(API_KEY);
+import firestore from '@react-native-firebase/firestore';
 
-    const checkUserExist = () => {
-      const currentUser = auth().currentUser;
-      if (currentUser) {
-        setEmail(currentUser.email.split('@')[0]);
-        // User is signed in
-      } else {
-        // No user is signed in
-        navigation.navigate('loginscreen');
-      }
-    };
-    
+
+const ChatAI = () => {
+    const [text, setText] = useState('')
+
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
+    const [displayName, setDisplayName] = useState('');
+    const [avatar, setAvatar] = useState('')
+
+    const [loadingResponse, setLoadingResponse] = useState(false);
+
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const AIImage = 'https://play-lh.googleusercontent.com/DDIUuR0XwdSLnuuyOTn3STuoemW_M1qCSLHs8HE6DJq0NrwUNxYafZ2qG-78Uxj76Q=w240-h480-rw'
+    useEffect(() => {
+      const fetchData = async () => {
+        if (user) {
+          const userDoc = await firestore()
+            .collection('users')
+            .doc(user.uid)
+            .get();
+          if (userDoc.exists) {
+            setDisplayName(userDoc.data().displayName);
+            setAvatar(userDoc.data().photoURL)
+          }
+        }
+        else{
+          navigation.navigate('loginscreen');
+        }
+      };
+  
+      fetchData();
+    }, []);
+
     const handleGenerateContent = async () => { 
-        checkUserExist()
-        try {
-            if(!prompt.trim()){
-                ToastAndroid.show('Hãy nhập prompt', ToastAndroid.SHORT)
-                return;
-            }
-            setLoading(true)
-            const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-            const result = await model.generateContent("You're a Mental Health Counselor at Pabcare Counseling Center " 
+        try {       
+          setLoadingResponse(true);
+          const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+          const result = await model.generateContent("You're a Mental Health Counselor at Pabcare Counseling Center " 
                                                         +"(Trung tâm hỗ trợ Pabcare) in Vietnamese."
                                                         +" Use friendly, funny and simple Vietnamese language to respond." 
                                                         +" Using 'tớ' to refer yourself and 'cậu' to refer me "
-                                                        +" This is a promt: " + prompt);
-            setText(result.response.text());
-            setPrompt("");
+                                                        +" This is a promt: " + messages);
+          return result.response.text();
         }
         catch (e){
           if (e.code === "SAFETY") {
@@ -49,89 +61,108 @@ const ChatAI = () => {
           }
         }
         finally{
-            setLoading(false)
+          setLoadingResponse(false);
         }
     }
+    
+  
+    const sendMessage = async () => {
+      setMessages(prevMessages => [
+        ...prevMessages,
+        { id: Math.random().toString(), text: newMessage, sender: displayName, avatar: avatar } 
+      ]);
+      setNewMessage('');
+      const aiResponse = await handleGenerateContent(newMessage);
+      
+      setMessages(prevMessages => [
+        ...prevMessages,
+        { id: Math.random().toString(), text: aiResponse, sender: 'Gemini', avatar: AIImage } 
+      ]);
+    };
+  
+    const isCurrentUser = (sender) => sender === displayName;
     return(
-        <ScrollView>
-        <View style={styles.container}>
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>Chat with AI {email}</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={messages}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <View style={[styles.messageContainer, isCurrentUser(item.sender) && styles.currentUserMessage]}>
+              {!isCurrentUser(item.sender) && (
+                <View style={styles.senderInfo}>
+                  <Image style={styles.avatar} source={{ uri: item.avatar }} />
+                  <Text style={styles.senderName}>{item.sender}</Text>
+                </View>
+              )}
+              <Text>{item.text}</Text>
+            </View>
+          )}
+        />
+        {loadingResponse && ( 
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size="small" color="#0000ff" />
           </View>
-  
-          <View style={styles.messageContainer}>
-            <Text style={styles.messageText}>{text}</Text>
-          </View>
-  
-          <View style={styles.messageRespond}>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Type your message..."
-              value={prompt}
-              onChangeText={setPrompt}
-            />
-  
-            <TouchableOpacity
-              style={styles.sendButton}
-              onPress={handleGenerateContent}
-              disabled={loading}
-            >
-              <MaterialIcons name="send" size={30} />
-            </TouchableOpacity>
-  
-            {loading && <ActivityIndicator style={styles.activityIndicator} />}
-          </View>
+        )}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={newMessage}
+            onChangeText={setNewMessage}
+            placeholder="Type your message here"
+          />
+          <Button title="Send" onPress={sendMessage} />
         </View>
-      </ScrollView>
+      </View>
     )
 }
 export default ChatAI
 
 const styles = StyleSheet.create({
     container: {
-      padding: 16,
-    },
-    titleContainer: {
-      marginBottom: 5,
-      backgroundColor: "#25db2e",
-      borderRadius: 10,
-      padding: 15,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-    messageContainer: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 8,
-      minHeight: 200,
-      borderRadius: 5,
-      marginBottom: 10
-    },
-    messageText: {
-      fontSize: 14,
-      fontWeight: "100",
-    },
-    messageRespond: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "100%",
-    },
-    textInput: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 8,
-      marginBottom: 8,
       flex: 1,
     },
-    sendButton: {
+    messageContainer: {
       padding: 10,
+      marginHorizontal: 10,
+      marginVertical: 5,
+      backgroundColor: '#e0e0e0',
+      borderRadius: 10,
+      maxWidth: '80%',
     },
-    activityIndicator: {
-      marginTop: 16,
+    currentUserMessage: {
+      alignSelf: 'flex-end',
+      backgroundColor: '#b3e5fc',
     },
+    senderInfo: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    avatar: {
+      width: 30,
+      height: 30,
+      borderRadius: 100,
+      marginRight: 5,
+    },
+    senderName: {
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      borderTopWidth: 1,
+      borderTopColor: '#ccc',
+    },
+    input: {
+      flex: 1,
+      marginRight: 10,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 20,
+    },
+    loadingContainer: {
+      alignItems: 'center',
+  },
   });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-    import {View, StyleSheet, ScrollView } from "react-native"
+    import {View, StyleSheet, Image, Pressable } from "react-native"
     import React, { useState, useEffect } from "react"
     import ChatAI from "../component/ChatAI"
     import RenderSliderImage from "../component/RenderSliderImage"
@@ -9,9 +9,25 @@
         useEffect(() => {
             fetchData();
         }, []);
+        const Personal = ()=>{
+            ToastAndroid.show('1', ToastAndroid.SHORT)
+        }
+          const Personal1W = ()=>{
+            ToastAndroid.show('2', ToastAndroid.SHORT)
+        }
+          const Personal1M = ()=>{
+            ToastAndroid.show('3', ToastAndroid.SHORT)
+        }
+
+        // const membershipData = [
+        //     { membershipName: 'Hour', imageSource: require('C://Users//ACER//OneDrive//Desktop//Resrource//personalHour.png'), func: Personal },
+        //     { membershipName: 'Week', imageSource: require('C://Users//ACER//OneDrive//Desktop//Resrource//personalWeek.png'), func: Personal1W },
+        //     { membershipName: 'Month', imageSource: require('C://Users//ACER//OneDrive//Desktop//Resrource//personalMonth.png'), func: Personal1M }
+    
+        // ];
 
         const fetchData = async () => {
-            try {
+            try { 
                 const response = await fetch('https://jsonplaceholder.typicode.com/photos');
                 const jsonData = await response.json();
                 const imageUrls = jsonData.slice(0, 4).map(item => item.url);
@@ -21,33 +37,68 @@
             }
         };
 
-        return(
-            <ScrollView style={styles.container}>
-                <ChatAI></ChatAI>
-                <View style>
-                    
+        return (
+            <View style={styles.container}>
+                <View style={styles.chatAI}>
+                    <ChatAI />
                 </View>
+                {/* <View style={styles.membership}>
+                    <View style={styles.memberBox}>
+                        <Text style={styles.membershipText}>99.000 vnđ/giờ</Text>
+                    </View>
+                    <View style={styles.memberBox}>
+                        <Text style={styles.membershipText}>499.000 vnđ/tuần</Text>
+                    </View>
+                    <View style={styles.memberBox}>
+                        <Text style={styles.membershipText}>1.399.000 vnđ/tháng</Text>
+                    </View>
+                </View> */}
+                {/* {membershipData.map((item, index) => (
+                <Pressable
+                    key={index}
+                    onPress={() => handlePress(item.func)} 
+                    style={styles.componentList}>
+                    <Image source={item.imageSource} style={styles.image} />
+                </Pressable>
+                ))} */}
                 <View style={styles.sliderImage}>
                     <RenderSliderImage images={images} />
                 </View>
-            </ScrollView>
+            </View>
         )
     }
     export default HomeScreen
-
+    
     const styles = StyleSheet.create({
-        container:{
-            flex:1,
-            //alignItems:"flex-start"
+        container: {
+            flex: 1,
+            // alignItems: 'center',
+            // justifyContent: 'center'
         },
-        membership:{
-            padding:15
+        membership: {
+            padding: 15,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginBottom: 20 // Add some space between membership and slider
         },
         sliderImage: {
-          paddingHorizontal: 15,
-          paddingVertical: 10,
-          width:400,
-          justifyContent: 'center', 
+            paddingHorizontal: 15,
+            paddingVertical: 10,
+            width: '100%',
+            alignItems: 'center'
         },
-
+        memberBox: {
+            backgroundColor: '#54595F',
+            padding: 10,
+            borderRadius: 5
+        },
+        membershipText: {
+            color: '#FFFFFF',
+            fontSize: 16,
+            fontWeight: 'bold',
+            textAlign: 'center'
+        },
+        chatAI: {
+            flex: 1
+        }
     })
